f3bead12815dc1c36ff87b5558fd17956a08e95f
Hitendra Rajput <hitendra.http@gmail.com>
2017-02-10 01:13:32 +0530

    Add sidebar

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import {testComp} from './testComp/testComp';
+import {sideBarComp} from "./sideBar/sideBarComp";
 
 angular.module('test.app')
-    .component(testComp.componentName, new testComp());
+    .component(testComp.componentName, new testComp())
+    .component(sideBarComp.componentName, new sideBarComp());
