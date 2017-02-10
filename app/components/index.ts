ee94bfc0d2106dd75e29277a8cd791fc2b798955
shreejibawa <shah.shreeji248@gmail.com>
2017-02-10 10:21:21 +0530

    minor modifications

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/index.d.ts" />
 
 import {testComp} from './testComp/testComp';
-import {sideBarComp} from "./sideBar/sideBarComp";
+import {sideBarComp} from './sideBar/sideBarComp';
 
 angular.module('test.app')
     .component(testComp.componentName, new testComp())
