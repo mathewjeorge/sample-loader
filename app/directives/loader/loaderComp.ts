7efcd4785806f84276a77049fff9443e40f1eef4
Hitendra Rajput <hitendra.http@gmail.com>
2017-03-05 13:03:58 +0530

    Enable showing custom message in loader.

--- app/directives/loader/loaderComp.ts
+++ app/directives/loader/loaderComp.ts
@@ -93,29 +93,31 @@ export class loader implements ng.IDirective {
 
             this.activeLoaders.push(loaderId);
 
-            let html: any;
+            let html: any = '';
 
-            if (config.message) {
-                html = `<div class="${config.messageClass}" id="${loaderId}">
-                            <div class="msg">${config.message}</div>
-                        </div>`;
-            } else if (config.loaderImage) { /** If specified loader image the prepare html accordingly else assig default loader */
+            if (config.loaderImage) { /** If specified loader image the prepare html accordingly else assig default loader */
                 if (!config.loaderClass) {
                     throw new Error("Loader class is required when specifying custom loader image."); // Throw exception if not provided loader class when using custom image
                 } else {
-                    html = `<div class="${config.loaderClass}" id="${loaderId}">
-                            <img src="${config.loaderImage}" alt="${config.loaderImageAlt ? config.loaderImageAlt : ''}" onerror="this.src='${config.fallbackImage ? config.fallbackImage : ''}'">
-                        </div>`;
+                    html += `<div class="${config.loaderClass}" id="${loaderId}">
+                            <img src="${config.loaderImage}" alt="${config.loaderImageAlt ? config.loaderImageAlt : ''}" onerror="this.src='${config.fallbackImage ? config.fallbackImage : ''}'">`;
+                    if (config.message) {
+                        html += `<div class="${config.messageClass}">${config.message}</div>`;
+                    }
+                    html += `</div>`;
                 }
             } else {
-                html = `
+                html += `
                 <div class="showbox" id="${loaderId}">
                     <div class="loader" style="width: ${config.size}px">
                         <svg class="circular" viewBox="25 25 50 50">
                             <circle class="path" style="stroke: ${config.color}" cx="50" cy="50" r="20" fill="none" stroke-width="${config.strokeWidth}" stroke-miterlimit="10"/>
                         </svg>
-                    </div>
-                </div>`;
+                    </div>`;
+                if (config.message) {
+                    html += `<div class="${config.messageClass}">${config.message}</div>`;
+                }
+                html += `</div>`;
             }
 
             loaderEle.append(html);
